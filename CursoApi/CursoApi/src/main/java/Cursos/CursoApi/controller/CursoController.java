91db0d8668197887b2ea799ac60cffc602d82de8
CandeGod <candebeatriz14@gmail.com>
2024-05-14 11:08:43 -0600

    Ya quedo el controller de curso

--- CursoApi/CursoApi/src/main/java/Cursos/CursoApi/controller/CursoController.java
+++ CursoApi/CursoApi/src/main/java/Cursos/CursoApi/controller/CursoController.java
@@ -16,5 +16,59 @@ import java.util.Optional;
 @RequestMapping("/curso")
 public class CursoController {
 
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    //Consulta general
+    @GetMapping()
+    public ResponseEntity<Iterable<Curso>> findAll(){
+        return ResponseEntity.ok(cursoRepository.findAll());
+    }
+
+
+    //Consulta por id
+    @GetMapping("/{idCurso}")
+    public  ResponseEntity<Curso> findById(@PathVariable Integer idCurso){
+        Optional<Curso> cursoOptional = cursoRepository.findById(idCurso);
+        if (!cursoOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cursoOptional.get());
+    }
+
+    //Crear un curso
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Curso curso, UriComponentsBuilder ucb){
+
+        Curso savedCurso = cursoRepository.save(curso);
+        URI uri = ucb
+                .path("curso/{idCurso}")
+                .buildAndExpand(savedCurso.getIdCurso())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+
+    //Actualizar un curso
+    @PutMapping ("/{idCurso}")
+    public ResponseEntity<Void> update (@PathVariable Integer idCurso, @RequestBody Curso curso){
+        Optional<Curso> cursoOptional = cursoRepository.findById(idCurso);
+        if (!cursoOptional.isPresent()){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        curso.setIdCurso(cursoOptional.get().getIdCurso());
+        cursoRepository.save(curso);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Borrar un curso
+    @DeleteMapping("/{idCurso}")
+    public ResponseEntity<Void> delete (@PathVariable Integer idCurso){
+        if (cursoRepository.findById(idCurso).get() == null){
+            return ResponseEntity.notFound().build();
+        }
+        cursoRepository.deleteById(idCurso);
+        return ResponseEntity.noContent().build();
+    }
     
 }
